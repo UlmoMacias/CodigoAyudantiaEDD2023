87f0d47ae5e478497d2003193620d69099d1c6bd
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-08-23 10:37:35 -0500

    Cree el repo con un archivo

---
+++ HelloWorld.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+
+        String a;
+        String b;
+        int x = 0;
+
+        /* La chicharronera 
+         * 
+         * -b +- sqrt(b^2 -4ac) / 2a
+         * 
+         * 
+        */
+        raiz = b^2 -4ac
+        dividendo = -b +- sqrt(raiz)
+        divisior = 2a
+        x = dividendo / divisior
+
+        System.out.println("Hola Mundo!!");
+        System.out.println(Arrays.toString(args));
+        System.out.println(args[0]);
+        for(){
+           asdea
+           asdaedasd
+           daerads 
+        }
+        asdaead
+
+    }
+
+    public static void foo(){
+        asdasdasdsad
+        asdasdasdsadasdasd
+        asdasdasdsadasdasd
+    }
+
+}
